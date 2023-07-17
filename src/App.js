46fa8460cc04a37499aa160d3d4b1d8f14bd9c0c
suryasardar <suryaprakashchary515@gamil.com>
2023-07-17 22:33:33 +0530

    done basic of navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,51 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Privateroute,
+  Products,
+  Singleproduct,
+} from "./pages";
+
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Navbar />
+          <Sidebar />
+          <Router exact path="/about">
+            <About />
+          </Router>
+          <Router exact path="/ cart">
+            <Cart />
+          </Router>
+          <Router exact path="/checkout">
+            <Checkout />
+          </Router>
+          <Router exact path="/">
+            <Home />
+          </Router>
+          <Router exact path="/privateroute">
+            <Privateroute />
+          </Router>
+          <Router exact path="/products">
+            <Products />
+          </Router>
+          <Router exact path="/*">
+            <Error />
+          </Router>
+          <Router exact path="/products/:id" children={<Singleproduct />} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 // console.log(1);
 
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./AboutPage.js";
+import Cart from "./CartPage.js";
+import Checkout from "./CheckoutPage.js";
+import Error from "./ErrorPage.js";
+import Home from "./HomePage.js";
+import Privateroute from "./PrivateRoute.js";
+import Products from "./ProductsPage.js";
+import Singleproduct from "./SingleProductPage.js";
+
+export {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Privateroute,
+  Products,
+  Singleproduct,
+};
